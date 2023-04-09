f615ff9dac2cd10d6e869c190a8b957c360450d0
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-10 03:37:53 +0530

    text-center

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,7 +2,7 @@ import { quotes } from "../assets";
 
 const Box = () => (
     <main className="grid lg:flex">
-        <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+        <div className="flex text-center justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
             <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
             Collab Workshops
             </h4>
@@ -10,15 +10,15 @@ const Box = () => (
             Our collab workshops bring together like-minded individuals who are passionate about entrepreneurship, innovation, and collaboration. Participants will work on real-world projects, learn from industry experts, and develop the skills they need to succeed in today's competitive business environment.
             </p>
         </div>
-        <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+        <div className="flex text-center  justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
             <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
             Incubation Programme
             </h4>
-            <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+            <p className="font-poppins  font-normal text-[18px] leading-[32.4px] text-white my-10">
             Our incubation programmes offer aspiring business owners the assistance and tools they need to start their own companies. Mentorship, training, funding, and access to other resources will be provided to participants in order to assist them in transforming their ideas into profitable businesses.
             </p>
         </div>
-        <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+        <div className="flex text-center  justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
             <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
             Internship Programs
             </h4>

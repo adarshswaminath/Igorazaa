bf19d22cd1584218c743c5df2c39bc13ba2aa1b5
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-10 02:49:27 +0530

    feature aligned

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -27,8 +27,8 @@ const CardDeal = () => (
     <div className="flex flex-wrap sm:justify-start justify-center items-center w-full feedback-container relative z-[1]">
       {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
     </div>
-    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1] text-center">
-            <h2 className={styles.heading2}>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col relative z-[1] text-center">
+    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white w-full">
                 Features
             </h2>
         </div>

6d9791c4e17f10c12d6676cd1e773f6b881ff8b7
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-10 15:35:49 +0530

    feedback-card rounded-lg

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -34,30 +34,30 @@ const Billing = () => (
   <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
   <div className="flex flex-wrap justify-center text-center">
     <div className="flex flex-wrap sm:w-full md:w-1/2">
-      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4">
+      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4 feedback-card rounded-lg">
         <div className=" rounded-lg p-6">
           <h2 className="text-2xl font-semibold mb-4 text-white">App Development</h2>
-          <p className="text-gray-600">We are always pushing the boundaries of what is possible and are committed to developing innovative products and services that are designed to meet the needs of our clients and customers</p>
+          <p className="text-gray-600 font-normal text-[18px] leading-[32.4px] my-10">We are always pushing the boundaries of what is possible and are committed to developing innovative products and services that are designed to meet the needs of our clients and customers</p>
         </div>
       </div>
-      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4">
+      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4 feedback-card rounded-lg">
         <div className=" rounded-lg p-6">
           <h2 className="text-2xl font-semibold mb-4 text-white">Website Design & Development</h2>
-          <p className="text-gray-600">We understand that every client and customer has unique needs, which is why we offer flexible solutions that can be customized to meet their specific requirements</p>
+          <p className="text-gray-600 font-normal text-[18px] leading-[32.4px] my-10">We understand that every client and customer has unique needs, which is why we offer flexible solutions that can be customized to meet their specific requirements</p>
         </div>
       </div>
     </div>
     <div className="flex flex-wrap sm:w-full md:w-1/2">
-      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4">
+      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4 feedback-card rounded-lg">
         <div className=" rounded-lg p-6">
           <h2 className="text-2xl font-semibold mb-4 text-white">Blockchain Development</h2>
-          <p className="text-gray-600">We are committed to putting our customers first. We work closely with our clients to understand their needs and provide them with the support and resources they need to succeed</p>
+          <p className="text-gray-600 font-normal text-[18px] leading-[32.4px] my-10">We are committed to putting our customers first. We work closely with our clients to understand their needs and provide them with the support and resources they need to succeed</p>
         </div>
       </div>
-      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4">
+      <div className="flex justify-center items-center w-full sm:w-1/2 md:w-full p-4 feedback-card rounded-lg">
         <div className=" rounded-lg p-6">
           <h2 className="text-2xl font-semibold mb-4 text-white">Website Reconstruct</h2>
-          <p className="text-gray-600">Our team of experts carefully design and develop our products or services using best practices and technologies. Rigorous testing procedures ensure early defect identification and maintenance of high quality.</p>
+          <p className="text-gray-600 font-normal text-[18px] leading-[32.4px] my-10">Our team of experts carefully design and develop our products or services using best practices and technologies. Rigorous testing procedures ensure early defect identification and maintenance of high quality.</p>
         </div>
       </div>
     </div>

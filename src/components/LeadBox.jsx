47f09d03093b5b7c2ab3525e9b187781ba409f0a
adarshswaminath <adarshswaminath7@gmail.com>
2023-04-12 01:10:59 +0530

    ceo social media removed

--- src/components/LeadBox.jsx
+++ src/components/LeadBox.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const LeadBox = () => (
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
     <div className="flex-1 flex flex-col">
-      <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full text-center text-[#e63946]">SparkLabs </h2>
+      <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full text-center text-[#ff2268]">SparkLabs </h2>
       <p className={`${styles.paragraph} mt-5 text-center`}>
       Igoraza is committed to help out clients 24×7. Feel free to reach out to us for any queries and help
       </p>
